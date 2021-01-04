8b5afd7c8b6248ebba5a7d66cb68d209a1096d1e
huyq119 <yuqi.hu@gmail.com>
2021-01-04 18:34:54 +0800

    胡玉琦作业内容

--- test_pytest/conftest.py
+++ test_pytest/conftest.py
@@ -8,6 +8,8 @@ import yaml
 import os
 
 # 通过 os.path.dirname 获取当前文件所在目录的路径
+from test_pytest.pythoncode.calculator import Calculator
+
 yaml_file_path = os.path.dirname(__file__) + "/data.yml"
 
 with open(yaml_file_path) as f:
@@ -17,23 +19,64 @@ with open(yaml_file_path) as f:
     add_datas = datas["datas"]
     # 获取文件中key为myids的数据
     add_ids = datas["myids"]
+    # 获取文件中key为datas_sub的数据
+    sub_datas = datas["datas_sub"]
+    # 获取文件中key为myids_sub的数据
+    sub_ids = datas["myids_sub"]
+    # 获取文件中key为datas_mul的数据
+    mul_datas = datas["datas_mul"]
+    # 获取文件中key为myids_mul的数据
+    mul_ids = datas["myids_mul"]
+    # 获取文件中key为datas_div的数据
+    div_datas = datas["datas_div"]
+    # 获取文件中key为myids_mul的数据
+    div_ids = datas["myids_div"]
 
-@pytest.fixture(params=add_datas, ids=add_ids)
+
+@pytest.fixture(params=add_datas, ids=add_ids, scope="module")
 def get_datas(request):
-    print("开始计算")
+    print("\n开始计算")
     data = request.param
-    print(f"request.param的测试数据是：{data}")
+    print(f"加法request.param的测试数据是：{data}")
     yield data
-    print("结束计算")
+    print("\n结束计算")
 
-@pytest.fixture(scope="session")
-def connectDB():
-    print("连接数据库操作")
-    yield
-    print("断开数据库连接")
 
-# @pytest.fixture(scope="class")
-# def get_calc():
-#     print("获取计算器实例")
-#     calc = Calculator()
-#     return calc
+@pytest.fixture(params=mul_datas, ids=mul_ids, scope="module")
+def get_muldatas(request):
+    print("\n开始计算")
+    data = request.param
+    print(f"乘法 request.param的测试数据是：{data}")
+    yield data
+    print("\n结束计算")
+
+
+@pytest.fixture(params=sub_datas, ids=sub_ids, scope="module")
+def get_subdatas(request):
+    print("\n开始计算")
+    data = request.param
+    print(f"减法 request.param的测试数据是：{data}")
+    yield data
+    print("\n结束计算")
+
+
+@pytest.fixture(params=div_datas, ids=div_ids, scope="module")
+def get_divdatas(request):
+    print("\n开始计算")
+    data = request.param
+    print(f"除法 request.param的测试数据是：{data}")
+    yield data
+    print("\n结束计算")
+
+
+# @pytest.fixture(scope="session")
+# def connectDB():
+#     print("连接数据库操作")
+#     yield
+#     print("断开数据库连接")
+
+@pytest.fixture(scope="module")
+def get_calc():
+    print("获取计算器实例")
+    calc = Calculator()
+    return calc
--- test_pytest/test_calc_new.py
+++ test_pytest/test_calc_new.py
@@ -8,14 +8,57 @@ import yaml
 class TestCalc:
 
     # 测试add函数
+    @pytest.mark.run(order=1)
     def test_add(self, get_calc, get_datas):
         result = None
         try:
             # 调用add函数,返回的结果保存在result里面
-            result = get_calc.add(get_datas[0],get_datas[1])
+            result = get_calc.add(get_datas[0], get_datas[1])
             # 判断result结果是否等于期望的值
             if isinstance(result, float):
                 result = round(result, 2)
         except Exception as e:
             print(e)
         assert result == get_datas[2]
+
+    # 测试mul函数
+    @pytest.mark.run(order=3)
+    def test_mul(self, get_calc, get_muldatas):
+        result = None
+        try:
+            # 调用mul函数,返回的结果保存在result里面
+            result = get_calc.mul(get_muldatas[0], get_muldatas[1])
+            # 判断result结果是否等于期望的值
+            if isinstance(result, float):
+                result = round(result, 2)
+        except Exception as e:
+            print(e)
+        assert result == get_muldatas[2]
+
+    # 测试sub函数
+    @pytest.mark.run(order=2)
+    def test_sub(self, get_calc, get_subdatas):
+        result = None
+        try:
+            # 调用sub函数,返回的结果保存在result里面
+            result = get_calc.sub(get_subdatas[0], get_subdatas[1])
+            # 判断result结果是否等于期望的值
+            if isinstance(result, float):
+                result = round(result, 2)
+        except Exception as e:
+            print(e)
+        assert result == get_subdatas[2]
+
+    # 测试div函数
+    @pytest.mark.run(order=4)
+    def test_div(self, get_calc, get_divdatas):
+        result = None
+        try:
+            # 调用div函数,返回的结果保存在result里面
+            result = get_calc.div(get_divdatas[0], get_divdatas[1])
+            # 判断result结果是否等于期望的值
+            if isinstance(result, float):
+                result = round(result, 2)
+        except Exception as e:
+            print(e)
+        assert result == get_divdatas[2]
